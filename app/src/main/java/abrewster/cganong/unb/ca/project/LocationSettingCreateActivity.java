2c9f09c29db8898acb83dc25a7023595c10b468d
AlexBrewster <abrewste@unb.ca>
2016-02-18 11:17:34 -0400

    Updated detail page, added create page

---
+++ app/src/main/java/abrewster/cganong/unb/ca/project/LocationSettingCreateActivity.java
@@ -0,0 +1,15 @@
+package abrewster.cganong.unb.ca.project;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LocationSettingCreateActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locationsetting_create);
+    }
+}
--- app/src/main/java/abrewster/cganong/unb/ca/project/LocationSettingListActivity.java
+++ app/src/main/java/abrewster/cganong/unb/ca/project/LocationSettingListActivity.java
@@ -5,21 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -56,8 +52,9 @@ public class LocationSettingListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Context context = view.getContext();
+                Intent intent = new Intent(context,LocationSettingCreateActivity.class);
+                context.startActivity(intent);
             }
         });
 
